27b2712c56f1cabd0098c0d0e74fb0d749354ba7
NNCRP <96888968+NNCRP@users.noreply.github.com>
2022-01-11 20:23:54 +0100

    Add files via upload

--- qb-sellitems/client/main.lua
+++ qb-sellitems/client/main.lua
@@ -13,21 +13,54 @@ local function DrawText3Ds(x, y, z, text)
     ClearDrawOrigin()
 end
 
+local scraplocation = vector3(-97.06, -1013.48, 27.28) 
+local sl = scraplocation --short version to make it easier (sl.x = x value of the vector3 and so on) 
+
+local druglocation = vector3(136.71, -1180.01, 29.79)
+local dl = druglocation
+
+
 CreateThread(function()
     while true do
         local InRange = false
         local PlayerPed = PlayerPedId()
         local PlayerPos = GetEntityCoords(PlayerPed)
 
-            local dist = #(PlayerPos - vector3(-96.97, -1013.49, 27.28)) 
+            local dist = #(PlayerPos - vector3(sl.x, sl.y, sl.z)) 
             if dist < 10 then
                 InRange = true
-                DrawMarker(2, -96.97, -1013.49, 27.28, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.2, 0.1, 255, 0, 0, 155, 0, 0, 0, 1, 0, 0, 0)
+                DrawMarker(2,sl.x, sl.y, sl.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.2, 0.1, 255, 0, 0, 155, 0, 0, 0, 1, 0, 0, 0)
                 if dist < 1 then
 
-                    DrawText3Ds(-96.97, -1013.49, 27.28, '~g~E~w~ - Sell resources') 
+                    DrawText3Ds(sl.x, sl.y, sl.z, '~g~E~w~ - Sell resources') 
                     if IsControlJustPressed(0, 38) then
-                        TriggerServerEvent('qb-sellitems:server:sell')
+                        TriggerServerEvent('qb-sellitems:server:sellscrap')
+                    end
+                end
+            end
+
+        if not InRange then
+            Wait(5000)
+        end
+        Wait(5)
+    end
+end)
+
+CreateThread(function()
+    while true do
+        local InRange = false
+        local PlayerPed = PlayerPedId()
+        local PlayerPos = GetEntityCoords(PlayerPed)
+
+            local dist = #(PlayerPos - vector3(dl.x, dl.y, dl.z)) 
+            if dist < 10 then
+                InRange = true
+                DrawMarker(2,dl.x, dl.y, dl.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.2, 0.1, 255, 0, 0, 155, 0, 0, 0, 1, 0, 0, 0)
+                if dist < 1 then
+
+                    DrawText3Ds(dl.x, dl.y, dl.z, '~g~E~w~ - Sell Drugs') 
+                    if IsControlJustPressed(0, 38) then
+                        TriggerServerEvent('qb-sellitems:server:selldrugs')
                     end
                 end
             end
--- qb-sellitems/server/main.lua
+++ qb-sellitems/server/main.lua
@@ -1,5 +1,5 @@
 local QBCore = exports['qb-core']:GetCoreObject()
-local ItemList = {
+local Scraplist = {
    
     ["metalscrap"]  =  10 ,
     ["plastic"]     =  20 ,
@@ -10,22 +10,59 @@ local ItemList = {
     ["glass"]       =  70 ,
 }
 
-RegisterNetEvent('qb-sellitems:server:sell', function()
+local Druglist = {
+
+    ["weed_white-widow"]    = 100,
+    ["weed_skunk"]          = 100,
+    ["weed_purple-haze"]    = 100,
+    ["weed_og-kush"]        = 100,
+    ["weed_amnesia"]        = 100,
+    ["weed_ak47"]           = 100,
+    ["cokebaggy"]           = 1000,
+
+}
+
+RegisterNetEvent('qb-sellitems:server:sellscrap', function()
     local src = source
     local price = 0
     local Player = QBCore.Functions.GetPlayer(src)
     
-    local xItem = Player.Functions.GetItemsByName(ItemList)
+    local xItem = Player.Functions.GetItemsByName(Scraplist)
     if xItem ~= nil then
         for k, v in pairs(Player.PlayerData.items) do
             if Player.PlayerData.items[k] ~= nil then
-                if ItemList[Player.PlayerData.items[k].name] ~= nil then
-                    price = price + (ItemList[Player.PlayerData.items[k].name] * Player.PlayerData.items[k].amount)
+                if Scraplist[Player.PlayerData.items[k].name] ~= nil then
+                    price = price + (Scraplist[Player.PlayerData.items[k].name] * Player.PlayerData.items[k].amount)
                     Player.Functions.RemoveItem(Player.PlayerData.items[k].name, Player.PlayerData.items[k].amount, k)
 
         Player.Functions.AddMoney("cash", price, "sold-resources")
             TriggerClientEvent('QBCore:Notify', src, "You sold your resources for $"..price)
-            TriggerEvent("qb-log:server:CreateLog", "sellitems", "resources", "blue", "**"..GetPlayerName(src) .. "** got $"..price.." for selling the resources")
+            TriggerEvent("qb-log:server:CreateLog", "sellscrap", "resources", "blue", "**"..GetPlayerName(src) .. "** got $"..price.." for selling the resources")
+                end
+            end
+        end
+    else
+        TriggerClientEvent('QBCore:Notify', src, "You have no resources..")
+    end
+
+end)
+
+RegisterNetEvent('qb-sellitems:server:selldrugs', function()
+    local src = source
+    local price = 0
+    local Player = QBCore.Functions.GetPlayer(src)
+    
+    local xItem = Player.Functions.GetItemsByName(Druglist)
+    if xItem ~= nil then
+        for k, v in pairs(Player.PlayerData.items) do
+            if Player.PlayerData.items[k] ~= nil then
+                if Druglist[Player.PlayerData.items[k].name] ~= nil then
+                    price = price + (Druglist[Player.PlayerData.items[k].name] * Player.PlayerData.items[k].amount)
+                    Player.Functions.RemoveItem(Player.PlayerData.items[k].name, Player.PlayerData.items[k].amount, k)
+
+        Player.Functions.AddMoney("cash", price, "sold-resources")
+            TriggerClientEvent('QBCore:Notify', src, "You sold your Drugs for $"..price)
+            TriggerEvent("qb-log:server:CreateLog", "selldrugs", "resources", "blue", "**"..GetPlayerName(src) .. "** got $"..price.." for selling the resources")
                 end
             end
         end
